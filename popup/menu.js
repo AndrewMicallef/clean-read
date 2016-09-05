d6e16ec0ee816935e109f27535ef9cba64b12610
amicallef <amicallef@student.unimelb.edu.au>
2016-09-05 18:50:03 +1000

    Menu implementation from MDN tutorial
    https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Your_second_WebExtension

--- popup/menu.js
+++ popup/menu.js
@@ -1,43 +1,28 @@
 /*
-Given the name of a beast, get the URL to the corresponding image.
-*/
-function beastNameToURL(beastName) {
-  switch (beastName) {
-    case "Frog":
-      return chrome.extension.getURL("beasts/frog.jpg");
-    case "Snake":
-      return chrome.extension.getURL("beasts/snake.jpg");
-    case "Turtle":
-      return chrome.extension.getURL("beasts/turtle.jpg");
-  }
-}
-
-/*
 Listen for clicks in the popup.
 
-If the click is not on one of the beasts, return early.
+If the click is not on one of the options, return early.
 
-Otherwise, the text content of the node is the name of the beast we want.
+Otherwise, the text content of the node is the name of option.
 
-Inject the "beastify.js" content script in the active tab.
+Inject the "clean-read.js" content script in the active tab.
 
-Then get the active tab and send "beastify.js" a message
-containing the URL to the chosen beast's image.
+Then get the active tab and send "clean-read.js" a message
+containing the opt.
 */
 document.addEventListener("click", function(e) {
-  if (!e.target.classList.contains("beast")) {
+  if (!e.target.classList.contains("option")) {
     return;
   }
 
-  var chosenBeast = e.target.textContent;
-  var chosenBeastURL = beastNameToURL(chosenBeast);
-
+  var choice = e.target.textContent;
+  
   chrome.tabs.executeScript(null, {
-    file: "/content_scripts/beastify.js"
+    file: "/content_scripts/clean-read.js"
   });
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {beastURL: chosenBeastURL});
+    chrome.tabs.sendMessage(tabs[0].id, {opt: choice});
   });
 
 });
